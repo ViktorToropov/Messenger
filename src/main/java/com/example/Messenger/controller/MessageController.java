7ce94817c11ff59e4653ba9310736c28f18088a3
ADMIN <tor-i51@yandex.ru>
2022-05-29 16:31:35 +0700

    add filter

--- src/main/java/com/example/Messenger/controller/MessageController.java
+++ src/main/java/com/example/Messenger/controller/MessageController.java
@@ -1,14 +1,20 @@
 package com.example.Messenger.controller;
 
+import com.example.Messenger.domain.Message;
+import com.example.Messenger.repos.MessageRepo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.Map;
 
 @Controller
 public class MessageController {
+    @Autowired
+    MessageRepo messageRepo;
 
     @GetMapping("/greeting")
     public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name,
@@ -18,7 +24,21 @@ public class MessageController {
     }
     @GetMapping
     public String main(Map<String,Object> model){
-        model.put("some","Hello, let's Code!");
+        Iterable<Message> messages = messageRepo.findAll();
+        model.put("messages",messages);
+        return "main";
+    }
+    @PostMapping
+    public String add(@RequestParam String text,@RequestParam String tag,Map<String,Object> model){
+        Message message = new Message(text,tag);
+        messageRepo.save(message);
+        Iterable<Message> messages = messageRepo.findAll();
+        model.put("messages",messages);
+        return "main";
+    }
+    @PostMapping
+    public String filter(@RequestParam String text,Map<String,Object> model){
+
         return "main";
     }
 
